80b9f6f0bb2d3bfff392fc707fbac41ecbe3b0d0
Yerassyl Saiman <113098449+yerass11@users.noreply.github.com>
2023-11-22 00:43:19 +0600

    upd 22.11

--- database/lab3.sql
+++ database/lab3.sql
@@ -1,123 +1,146 @@
---1
---a.
-select title from course
-where dept_name = 'Biology' and credits > 3;
+create database ICT;
+drop database ICT;
 
---b.
-select room_number from classroom
-where building = 'Watson' or building = 'Painter';
+CREATE TABLE Students01(
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    courses INTEGER,
+    date_of_birth DATE,
+    age INTEGER,
+    gpa DECIMAL
+);
+drop table Students01;
 
---c.
-select course_id from course
-where dept_name = 'Comp. Sci.';
-
---d.
-select course_id from section
-where semester = 'Spring';
-
---e.
-select name from student
-where tot_cred > 45 and tot_cred < 85;
-
---f.
-select course_id, title from course
-where lower(right(title, 1)) in ('a', 'e', 'i', 'o', 'u', 'y');
-
---g.
-SELECT title FROM course
-WHERE course_id IN (SELECT course_id FROM prereq WHERE prereq_id = 'EE-181');
- --2
- --a.
-select  dept_name, avg(salary) as avg_salary
-from instructor group by dept_name
-order by avg_salary ASC;
--- order by dept_name desc;
---b.
-select building
-from section
-group by building
-order by count(*) desc
-limit 1;
-
---c.
-select dept_name, count(*)
-from course
-group by dept_name
-order by count(*);
-
---d.
-select id, name from student
-where dept_name = 'Comp. Sci.' and tot_cred > 3;
-
---e.
-select id, name from instructor
-where dept_name in(
-    select dept_name
-    from department
-    where building = 'Taylor'
-    );
-
---f.
-select name from instructor
-where dept_name = 'Biology' or dept_name = 'Music' or dept_name = 'Phylosophy';
-
---g.
-select name from instructor
-where id in(
-    select id
-    from teaches
-    where year = '2018' and year != '2023'
-    );
-
---3
---a.
-select name from student
-where dept_name = 'Comp. Sci.' and id in (
-    select id
-    from takes
-    where grade = 'A' or grade ='A-'
-    )order by name ASC;
+INSERT INTO Students01(student_id, first_name, last_name, courses, date_of_birth, age, gpa)
+VALUES
+    (1, 'Yerassyl', 'Saiman', 2, '2005-05-01', 18, 3.6),
+    (2, 'Garcia', 'Johnson', 2, '2005-11-30', 18, 4.0),
+    (3, 'Robert', 'Williams', 2, '2004-11-25', 18, 3.8),
+    (4, 'Daniel', 'Brown', 2, '2005-03-22', 18, 2.5),
+    (5, 'Michael', 'Davis', 2, '2005-03-08', 18, 3.5),
+    (6, 'William', 'Miller', 1, '2004-08-23', 19, 3.0),
+    (7, 'Sarah', 'Wilson', 3, '2005-11-30', 18, 2.9),
+    (8, 'Benjamin', 'Moore', 2, '2002-02-17', 21, 3.4),
+    (9, 'Emily', 'Sula', 4, '2003-07-09', 20, 3.3);
 
 
---b.
-select name from instructor
-where id in (
-    select i_id
-    from advisor
-    where s_id in (
-        select id
-        from takes
-        where grade = 'B'
-        )
-    );
+--a. Write SQL queries to select all students of a certain course.
+SELECT *
+FROM Students01
+WHERE courses = 2;
 
---c.
-select dept_name from student
-where id in(
-    select id
-    from takes
-    where grade != 'F' and grade != 'C'
-    );
+--b. Select and output the most senior and junior students.
+SELECT * FROM students01
+WHERE date_of_birth = (SELECT max(date_of_birth) FROM students01)
+OR date_of_birth = (SELECT min(date_of_birth) FROM students01);
 
---d.
-select name from instructor
-where id in (
-    select id
-    from teaches
-    where course_id in (
-        select course_id
-        from takes
-        where grade != 'A' and grade != 'A-'
-        )
-    );
+--c. Write a request that will update the information about the student (for example, change his last name).
+UPDATE Students01
+SET last_name = 'Saiman'
+WHERE last_name = 'Saimanov';
 
-select title from course
-where course_id in(
-    select course_id
-    from section
-    where time_slot_id in (
-        select time_slot_id
-        from time_slot
-        where start_hr < 13
-        )
-    );
+UPDATE Students01
+SET last_name = 'Saimanov'
+WHERE last_name = 'Saiman';
+
+--d. Apply changes to multiple students.
+UPDATE Students01
+SET age = 20
+Where age = 19;
+
+
+--Create some more tables (for example, "Teachers" and "Courses") and link them with relationships.
+-- Create the Teachers table with a composite unique constraint.
+CREATE TABLE Teachers(
+    id_teacher SERIAL PRIMARY KEY,
+    name_teacher VARCHAR(255),
+    UNIQUE (id_teacher, name_teacher)
+);
+
+INSERT INTO TEACHERS(id_teacher, name_teacher)
+VALUES
+    (1, 'TIMUR MUSTAFIN'),
+     (2, 'ARNUR KELGENBAEV');
+
+SELECT * FROM TEACHERS;
+
+-- Create the Courses table with a composite unique constraint.
+CREATE TABLE Courses(
+    id_course SERIAL PRIMARY KEY,
+    name_course VARCHAR(255),
+    UNIQUE (id_course, name_course)
+);
+
+INSERT INTO Courses(id_course, name_course)
+VALUES
+    (1, 'CALCULUS'),
+     (2, 'PP1');
+
+SELECT * FROM Courses;
+
+DROP TABLE Teachers;
+DROP TABLE COURSES;
+-- Add a unique constraint on "studentsId" and "firstName" in the Students01 table.
+ALTER TABLE Students01
+ADD CONSTRAINT unique_student_id_name UNIQUE (student_id, first_name);
+
+
+CREATE TABLE StudentCourse(
+    idTeacher INT,
+    idCourse INT,
+    studentsId INT,
+    nameTeacher VARCHAR(255),
+    nameCourse VARCHAR(255),
+    firstName VARCHAR(255),
+    PRIMARY KEY (idTeacher, idCourse, studentsId),
+    FOREIGN KEY (idTeacher, nameTeacher) REFERENCES Teachers(id_teacher, name_teacher),
+    FOREIGN KEY (idCourse, nameCourse) REFERENCES Courses(id_course, name_course),
+    FOREIGN KEY (studentsId, firstName) REFERENCES Students01(student_id, first_name)
+);
+
+DROP TABLE StudentCourse;
+--a. Write a query that returns the number of students in each course.
+SELECT courses, count(*) as num
+FROM Students01
+GROUP BY courses;
+
+--b. Write a query to calculate the average age of students in each course.
+
+SELECT courses, avg(age)
+FROM Students01
+GROUP BY courses;
+
+--c. Write a query to select students who have an average score above the average score for all students
+SELECT AVG(gpa) FROM Students01;
+
+SELECT *
+FROM Students01
+WHERE gpa > (SELECT AVG(gpa) FROM Students01);
+
+
+--d. Write a request to select students who do not have grades below
+SELECT *
+FROM Students01
+WHERE gpa >= 3.0;
+
+-- 3.
+--Write an SQL query that will create a view that combines data about students, courses and their grades.
+CREATE VIEW StudentCourseGrades AS
+SELECT s.student_id, s.first_name, s.last_name, s.courses, s.gpa
+FROM Students01 s
+JOIN Courses AS c ON s.courses = c.id_course;
+
+SELECT * FROM StudentCourseGrades;
+
+drop view StudentCourseGrades;
+--• Write a query that will output the average score of students in each course from the created view.
+
+SELECT courses, AVG(gpa)
+FROM StudentCourseGrades
+GROUP BY courses;
+
+
+
+DROP table Students01;
+DROP table Teachers;
